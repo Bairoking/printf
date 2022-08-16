9c81f14ed7c4aeeeccf6c57ddb036c63dd289ca2
Bairoking <gs8004rtj5jj5j@gmail.com>
2022-08-16 08:37:45 -0700

    Team project

--- _printf.c
+++ _printf.c
@@ -1,8 +1,6 @@
 #include <unistd.h>
 #include <stdarg.h>
 #include "main.h"
-
-
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -10,51 +8,51 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
 */
-
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
-
 void print_string(char *str)
 {
 	int i = 0;
-	do {
-		_putchar( str[i]);
-		i++;
-	} while (str[i] != '\0');
-}
 
-/*void write_int(int n)
+	do()	{
+		_putchar(str[i]);
+		i++;
+}
+	while (str[i] != '\0');
+}
+void write_int(int n)
 {
-	int tmp = n, i = 0, j = 0; 
+	int tmp = n, i = 0, j = 0;
+
 	while (n % 10 > 9)
 	{
 		i++;
 		tmp = tmp % 10;
 	}
-	
 	for (j = tmp; j > 0; j--)
 		_putchar(48 + (n % (int)pow(10, tmp)));
 	_putchar(48 + tmp);
-}*/
-
+}
 /**
  * _printf - Produces output according to a format
  * @format: The format of the string.
  * Return: The number of characters printed
  */
 int _printf(const char *format, ...)
-{	
+{
 	va_list ap;
-	unsigned int j = 0, n = 0; /*i = 0;*/
-	
-	/*while (format[i] != '\0')
-	{	
+	unsigned int j = 0;
+	unsigned int n = 0;
+	unsigned int i = 0;
+
+	while (format[i] != '\0')
+	{
 		if (format[i] == '%')
 			n++;
 		i++;
-	}*/
+	}
 	va_start(ap, format);
 	while (format[j] != '\0')
 	{
@@ -72,16 +70,15 @@ int _printf(const char *format, ...)
 					_putchar('%');
 					break;
 			}
-
-			/*if (format[j + 1] == 'c')
+			if (format[j + 1] == 'c')
 				_putchar(va_arg(ap, int));
 			else if (format[j + 1] == 's')
-				print_string(va_arg(ap, char *));*/
+				print_string(va_arg(ap, char *));
 			j++;
 		}
-		else 
+		else
 		{
-			_putchar(format[j]); 
+			_putchar(format[j]);
 		}
 		j++;
 	}
